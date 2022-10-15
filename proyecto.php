47dc3b055b5eeb73c1faa10691060f674b8b7a49
LeZoppi <67881313+LeZoppi@users.noreply.github.com>
2022-10-14 21:20:13 -0300

    mod proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,22 +1,26 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Perfil de Proyecto</title>
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <meta charset="utf-8">
-    <link href="css/estilos.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" />
     <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
-
+    <!-- Traido del php inicio-->
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
 <body>
-    
-
-<!--  caja de comentarios para todos los proyectos  -->
-    <?php 
-    
+<?php 
     require ('conexion.php');
 
     $idproyecto=$_GET['id'];
@@ -28,16 +32,99 @@
         $moneda_proyecto=$r['moneda_proyecto'];
         $imagen_proyecto=$r['imagen_proyecto'];
         $idred=$r['idred'];
+
+    
     }
+
+    $idusuario =$_SESSION['logueado'];
+    $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE idusuario=$idusuario");
+
+    while($respuesta=mysqli_fetch_assoc($consulta)){
+
+      $nick = $respuesta['nickname'];
+      $email = $respuesta['email'];
+      $descripcion = $respuesta['descripcion']; 
+      $twitter = $respuesta['twitter'];
+      $instagram = $respuesta['instagram'];
+      $facebook = $respuesta['facebook'];
+      
+      
+    }
+
     ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4><?php echo $nombre_proyecto; ?></h4>
+    <!-- Barra Horizontal-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand"  href="#page-top"> <img src=" assets/img/sad.png" alt="..." /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="./admin/redeslist.php">Redes</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./admin/proyectoslist.php">Proyectos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://es.investing.com/news/cryptocurrency-news">Noticias</a></li>
+                        <li class="nav-item"><a class="nav-link" href="perfil.php">Perfil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="tienda.php">Tienda</a></li>
+                        
+                    </ul>
+                </div>
             </div>
+        </nav>
+        <!-- Declaracion del nombre del proyecto mas el simbolo-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-heading text-uppercase"><?php echo $nombre_proyecto;   ?></div>
+                <div class="masthead-heading text-uppercase"><?php echo $moneda_proyecto; ?></div>
+                
+                
+            </div>
+        </header>
+                                <!-- Imagenes descripciones y mas -->
+        <section class="page-section" >
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Nuestros servicios</h2>
+                    <h3 class="section-subheading text-muted">Primer foro enfocado a juegos NFT</h3>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Catalogo de Redes y Proyectos</h4>
+                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Recompensas por Interactuar</h4>
+                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Ultimas Noticias del Mundo Cripto</h4>
+                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<!--  caja de comentarios para todos los proyectos  -->
+    
+    <div class="container">
+        <div class="comment-form-container">
+            <div class="container"> <h4><?php echo $nombre_proyecto;  echo ' le recuerda ser respestuoso con los otros usuarios' ?></h4></div>
         </div>
-        <div class="row">
-            <div class="col-md-6">
+        <div class="container">
+            <div class="container">
                 <div class="panel-body">
 
                     <!--Inicio elementos contenedor-->
@@ -46,7 +133,7 @@
                         
                         <form id="frm-comment">
                             <div class="input-row">
-                                <input type="hidden" name="comentario_id" id="commentId" placeholder="Name" /> <input class="input-field" type="text" name="name" id="name" placeholder="Nombres" />
+                                <input type="hidden" name="comentario_id" id="commentId" placeholder="Name" /> <input class="input-field" type="text" name="name" value = "<?php echo $nick;   ?> "id="name" placeholder= <?php echo $nick;   ?> readonly />
                             </div>
                             <div class="input-row">
                                 <textarea class="input-field" type="text" name="comment" id="comment" placeholder="Agregar comentario">  </textarea>
@@ -260,5 +347,23 @@
     
 
 </body>
+
+ <!-- Footer-->
+ <footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 text-lg-start"> 2022</div>
+                    <div class="col-lg-4 my-3 my-lg-0">
+                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <div class="col-lg-4 text-lg-end">
+                        <a class="link-dark text-decoration-none me-3" href="#!">Politicas</a>
+                        <a class="link-dark text-decoration-none" href="#!">Terminos de Uso</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
 </html>
