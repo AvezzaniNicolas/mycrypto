f4308a226acc2b7a5c2b718bd65b008b26b6a4f7
LeZoppi <67881313+LeZoppi@users.noreply.github.com>
2022-10-26 03:53:37 -0300

    Nuevos proyectos

--- mycrypto.sql
+++ mycrypto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-10-2022 a las 08:30:41
+-- Tiempo de generación: 26-10-2022 a las 08:52:37
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
