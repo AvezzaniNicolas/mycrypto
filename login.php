0e7655e5ed1f30605c364fb8043d1fd577885d2f
Nicolás avezzani <nicolas.avezzanirc@gmail.com>
2023-06-22 16:37:23 -0300

    hola
    a

--- login.php
+++ login.php
@@ -20,6 +20,33 @@ if (isset($_POST['ingresar']) && !empty ($_POST['ingresar']) ){
                    $idrol=$r['idrol'];              
 
                 }
+                // Asignar monedas al usuario que inició sesión
+            $idusuario = $respuesta['idusuario'];
+
+            // Obtener la cantidad de monedas actual del usuario desde la base de datos
+            $sql = "SELECT moneda FROM inventarios WHERE idusuario = '$idusuario'";
+            $result = mysqli_query($conexion, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                $cantidadMonedas = $row['moneda'];
+            } else {
+                // Si el usuario no tiene un registro en la tabla de inventarios, asignar un valor predeterminado
+                $cantidadMonedas = 0;
+            }
+
+            // Verificar si la cantidad de monedas es 0 y agregar 100 monedas al usuario
+            if ($cantidadMonedas == 0) {
+                $cantidadMonedas += 100;
+
+                // Actualizar la cantidad de monedas en la base de datos
+                $updateSQL = "UPDATE inventarios SET moneda = '$cantidadMonedas' WHERE idusuario = '$idusuario'";
+                if (mysqli_query($conexion, $updateSQL)) {
+                    echo "Se agregaron $cantidadMonedas monedas al usuario con ID $idusuario<br>";
+                } else {
+                    echo "Error al actualizar las monedas del usuario con ID $idusuario: " . mysqli_error($conexion);
+                }
+            }
             $_SESSION['rol'] = $idrol;
             $_SESSION['logueado'] = $respuesta['idusuario'];       
             header ("location: inicio.php");
