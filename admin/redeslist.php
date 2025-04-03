20ad978639e46376edc5c810efa7c8050202065e
LeZoppi <67881313+LeZoppi@users.noreply.github.com>
2025-04-03 14:11:49 -0300

    cambios

--- admin/redeslist.php
+++ admin/redeslist.php
@@ -7,6 +7,22 @@ $active_banner="active";
 
 session_start();
 ?>
+
+
+<?php 
+require ('../conexion.php');
+$idusuario = $_SESSION['logueado'];
+$consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE idusuario=$idusuario");
+while($respuesta = mysqli_fetch_assoc($consulta)) {
+    $nick = $respuesta['nickname'];
+    $email = $respuesta['email'];
+    $nombre = $respuesta['nombre'];
+    $apellido = $respuesta['apellido'];
+    $foto = $respuesta['foto'] ?? 'default.jpg';
+    $rol = $_SESSION['rol'];
+}?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -24,21 +40,43 @@ session_start();
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <!-- Custom styles for this template -->
     <link href="css/proyectlist.css" rel="stylesheet">
+	<link href="css/perfil.css" rel="stylesheet">
   </head>
+
+  
+  <style>
+        /* Estilos adicionales */
+	
+        .btn {
+            background:rgb(212, 175, 55);
+            color: white;
+            border: none;
+            padding: 10px 15px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        
+        .btn:hover {
+            background:rgb(0, 0, 0);
+        }
+    </style>
   <body style="background: #212529;" >
+
+  <!-- Barra de navegación -->
+<div class="navbar">
+    <a href="inicio.php" class="active">Inicio</a>
+    <a href="https://es.cointelegraph.com/tags/games">NOTICIAS</a>
+    <a href="proyectoslist.php">PROYECTOS</a>
+    <a href="tienda.php">TIENDA</a>
+	<a href="../perfil.php">PERFIL</a>
+    
+    <div class="user-info">
+        <span>Bienvenido, <?php echo $nick; ?></span>
+        <span>(ID ROL:<?php echo $rol; ?>)</span>
+        <a href="logout.php" style="color:rgb(0, 0, 0);">Cerrar Sesión</a>
+    </div>
+</div>
 	
-	<?php /*include("top_menu.php");*/?>
-	
-    <div class="container">
-		
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="row">
-	  <div class="breadcrumb-container">
-		 <ol class="breadcrumb">
-		 	<li><a href="../inicio.php" class="btn btn-warning" role="button">Volver al menu de inicio</a></li>
-		  	<li><a href="proyectoslist.php"class="btn btn-warning" role="button">ir a juegos</a></li>
-		  	<li><a href="../perfil.php"class="btn btn-warning" role="button">ir a mi perfil</a></li>
-		</ol>
 		
 				<?php 
 				if(isset($_SESSION['logueado']) && $_SESSION['logueado']>0){
@@ -109,6 +147,22 @@ session_start();
     <!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
+  <!-- Footer-->
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-start">Copyright &copy; MyCrypto 2022</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="https://github.com/AvezzaniNicolas/mycrypto/tree/main" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <br>
+            <div class="col-lg-4 text-lg-end">
+                <a class="link-dark text-decoration-none me-3" href="#!">Politica de Privacidad</a>
+                <a class="link-dark text-decoration-none" href="#!">Terminos y Condiciones</a>
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
 <script>
 	$(document).ready(function(){
