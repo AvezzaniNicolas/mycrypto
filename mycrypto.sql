a166430be3dff2e0b1ad8eb05fd0d3eb34b01441
LeZoppi <67881313+LeZoppi@users.noreply.github.com>
2025-04-13 12:26:43 -0300

    Add files via upload

--- mycrypto.sql
+++ mycrypto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-04-2025 a las 16:45:30
+-- Tiempo de generación: 13-04-2025 a las 17:26:32
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
